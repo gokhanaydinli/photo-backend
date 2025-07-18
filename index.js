f0f254f3d99d50ad27d65562d3737a4be460e3c6
gokhanaydinli <gokhan.aydinli@rapsodo.com>
2025-07-18 10:18:29 +0300

    index.html eklendi

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
 const uploadDir = 'uploads';
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
